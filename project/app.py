4dc48c684894c125de4f279d7d5a23563ffacc24
Himanshu Kumar <himanshukumar14092003@gmail.com>
2025-01-17 14:02:22 +0530

    Update app.py
    connected mongoDB atlas

--- project/app.py
+++ project/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, render_template, jsonify
 from googleapiclient.discovery import build
 from textblob import TextBlob
+from pymongo import MongoClient
 
 API_KEY = 'Your_API'
 
 app = Flask(__name__)
 
+client = MongoClient("mongodb+srv://<your_username>:<your_pass>@cluster0.q6klp.mongodb.net/")
+db = client['YouTubeAnalyzer']
+contact_collection = db['ContactUs'] 
 @app.route('/')
 def index():
     return render_template('index.html', comments=None, positivePercentage=0, neutralPercentage=0, negativePercentage=0)
@@ -53,6 +57,21 @@ def analyze_sentiments(comments):
         'neutral': neutral_percentage,
         'negative': negative_percentage
     }
+@app.route('/contact', methods=['POST'])
+def contact_us():
+    
+    data = request.json 
+    name = data.get('name')
+    email = data.get('email')
+    message = data.get('message')
+
+    contact_collection.insert_one({
+        "name": name,
+        "email": email,
+        "message": message
+    })
+    
+    return jsonify({"message": "Thank you for reaching out!"}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
